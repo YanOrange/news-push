e5d158b570b38f6ee2771c2d7f05cc26379b8057
YanOrange <15340795332@163.com>
2021-03-12 16:28:40 +0800

    Initial commit

---
+++ src/main/java/com/delay/newspush/NewsPushApplication.java
@@ -0,0 +1,13 @@
+package com.delay.newspush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsPushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsPushApplication.class, args);
+    }
+
+}
